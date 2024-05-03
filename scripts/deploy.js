5ff6753bfefb5f3236e3c5867c0e7f3c5313b2f6
Nucoco <yuuta11511997@gmail.com>
2024-05-03 18:14:48 +0900

    contract call via proxy

---
+++ scripts/deploy.js
@@ -0,0 +1,41 @@
+// deploy.js
+async function main() {
+  // logic1: bankV1
+  const bankV1ContractFactory = await hre.ethers.getContractFactory("BankV1");
+  const bankV1 = await bankV1ContractFactory.deploy();
+  await bankV1.waitForDeployment();
+  const bankV1Addr = await bankV1.getAddress();
+  console.log("Bank1:", bankV1Addr);
+
+  // storage: upgradeProxy
+  const proxyContractFactory = await hre.ethers.getContractFactory("UpgradeProxy");
+  const proxyContract = await proxyContractFactory.deploy(bankV1Addr);
+  await proxyContract.waitForDeployment();
+  const proxyAddr = await proxyContract.getAddress();
+  console.log("Proxy:", proxyAddr);
+
+  // call setName via Proxy
+  const [signer] = await hre.ethers.getSigners();
+  const iface = bankV1.interface;
+  // const iface = new hre.ethers.Interface(["function setName(string newName)", "function deposit() payable"]);
+  // console.log(iface)
+
+  let abiEncodedCall;
+  abiEncodedCall = iface.encodeFunctionData("setName", [ "piggy bank" ]);
+	console.log(`calldate of setName: ${abiEncodedCall}`)
+
+  const tx = await signer.sendTransaction({
+    to: proxyAddr,
+    data: abiEncodedCall,
+  });
+  console.log("Tx Hash:", tx.hash);
+
+  // getname
+  console.log(`calldate of setName: ${abiEncodedCall}`)
+  // console.log('proxy.name: ', await proxyContract.callStatic.name());
+};
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
